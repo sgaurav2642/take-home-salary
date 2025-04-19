7d35bc4e10fbc09ff95ef526aa8a6e186214c1a8
Anu <sgaurav2642@gmail.com>
2025-04-19 22:26:26 +0530

    modifyied port details

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def calculate_salary():
     return jsonify(monthly_salary=monthly_salary)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
 
 
 
